86aae135419139c519ee11c67e9be6b531f7e6bb
TB831 <druu831@gmail.com>
2018-07-24 12:19:51 -0700

    action creator setup

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,10 @@
 import config from '../../config.js';
 const API_KEY = config.API_KEY;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+// Action creators always return an object
+export function fetchWeather() {
+  return {
+    type: FETCH_WEATHER
+  };
+}
