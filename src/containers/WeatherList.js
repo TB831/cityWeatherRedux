d126475ecbbccc85a2ad4ead834f728f1362d033
TB831 <druu831@gmail.com>
2018-07-25 12:02:29 -0700

    WeatherList now allows users to add cities

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    return (
+      <tr>
+        <td>{cityData.city.name}</td>
+      </tr>
+    );
+  }
+
   render() {
     return (
       <table className='table table-hover'>
@@ -14,14 +22,14 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
   }
 }
 
-const mapStateToProps = (state => {
+const mapStateToProps = (state) => {
   return { weather: state.weather };
 }
 
